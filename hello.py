45a33efdbf81a744d3e301f9f6e61d235010a482
Marwan Tabbara <marwan.tabbara@outlook.com>
2024-09-04 15:55:00 -0400

    Modification hello py

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+aire = math.pi * rayon ** 2
+print(aire)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print("Marwan Tabbara")
